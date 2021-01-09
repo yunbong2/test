36c5c76969fd49f750be5903e1256f816d1d2beb
yunbong2 <yunbong2@users.noreply.github.com>
2021-01-09 18:19:10 +0900

    gap switch

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -844,11 +844,13 @@ static void bb_ui_draw_UI(UIState *s)
   const int bb_dmr_y = (box_y + (bdr_is * 1.5)) + UI_FEATURE_RIGHT_Y;
 
 #if UI_FEATURE_LEFT
-  bb_ui_draw_measures_left(s, bb_dml_x, bb_dml_y, bb_dml_w);
+  if (s->SccSmootherSwitchGapOnly == 1)
+    bb_ui_draw_measures_left(s, bb_dml_x, bb_dml_y, bb_dml_w);
 #endif
 
 #if UI_FEATURE_RIGHT
-  bb_ui_draw_measures_right(s, bb_dmr_x, bb_dmr_y, bb_dmr_w);
+  if (s->SccSmootherSwitchGapOnly == 1)
+    bb_ui_draw_measures_right(s, bb_dmr_x, bb_dmr_y, bb_dmr_w);
 #endif
 
 #if UI_FEATURE_DEBUG
