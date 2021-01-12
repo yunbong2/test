e8d5a638f025e016f10f91fd707c2633c1253165
neokii <neokii@gmail.com>
2021-01-12 10:46:28 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -178,9 +178,9 @@ class CarInterface(CarInterfaceBase):
       '''
 
       ret.longitudinalTuning.kpBP = [0., 10., 30., 40.]
-      ret.longitudinalTuning.kpV = [1.0, 0.7, 0.5, 0.3]
+      ret.longitudinalTuning.kpV = [0.9, 0.6, 0.4, 0.3]
       ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
-      ret.longitudinalTuning.kiV = [0.05, 0.02, 0.01, 0.005]
+      ret.longitudinalTuning.kiV = [0.03, 0.015, 0.01, 0.005]
       ret.longitudinalTuning.kf = 0.5
       ret.longitudinalTuning.deadzoneBP = [0., 40]
       ret.longitudinalTuning.deadzoneV = [0., 0.02]
