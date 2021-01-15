e81e148a9aaf7e8c729ce0c1adb8e56501e98de2
neokii <neokii@gmail.com>
2021-01-15 11:39:48 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -187,7 +187,7 @@ class CarInterface(CarInterfaceBase):
 
       ret.gasMaxBP = [0., 30.*CV.KPH_TO_MS, 60.*CV.KPH_TO_MS]
       ret.gasMaxV = [0.4, 0.3, 0.2]
-      ret.brakeMaxBP = [0., 60.*CV.KPH_TO_MS]
+      ret.brakeMaxBP = [0., 70.*CV.KPH_TO_MS]
       ret.brakeMaxV = [0.95, 0.85]
 
       ret.stoppingBrakeRate = 0.2  # brake_travel/s while trying to stop
