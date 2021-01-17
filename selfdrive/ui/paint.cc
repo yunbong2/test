6350ca9df249025e1b37087db6c297b3f9a6c527
yunbong2 <yunbong2@users.noreply.github.com>
2021-01-17 22:53:16 +0900

    lane fix

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -771,16 +771,16 @@ static void ui_draw_debug(UIState *s)
   nvgFontFace(s->vg, "sans-semibold");
   nvgFillColor(s->vg, COLOR_WHITE_ALPHA(150));
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
-  ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+650 + y_gain, "커브");
+  ui_print(s, ui_viz_rx_center, ui_viz_ry+650, "커브");
   if (scene.curvature >= 0.001) {
-    ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+700 + y_gain, "↖%.4f　", abs(scene.curvature));
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "↖%.4f　", abs(scene.curvature));
   } else if (scene.curvature <= -0.001) {
-    ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+700 + y_gain, "　%.4f↗", abs(scene.curvature));
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "　%.4f↗", abs(scene.curvature));
   } else {
-    ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+700 + y_gain, "　%.4f　", abs(scene.curvature));
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "　%.4f　", abs(scene.curvature));
   }
-  ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+750 + y_gain, " 좌측간격(m)       차선폭(m)       우측간격(m)");
-  ui_print(s, ui_viz_rx_center + x_gain, ui_viz_ry+800 + y_gain, "%.2f                       %.2f                       %.2f", scene.pathPlan.lPoly, scene.pathPlan.laneWidth, abs(scene.pathPlan.rPoly));
+  ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " 좌측간격(m)       차선폭(m)       우측간격(m)");
+  ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%.2f                       %.2f                       %.2f", scene.pathPlan.lPoly, scene.pathPlan.laneWidth, abs(scene.pathPlan.rPoly));
 }
 
 static void bb_ui_draw_debug(UIState *s)
